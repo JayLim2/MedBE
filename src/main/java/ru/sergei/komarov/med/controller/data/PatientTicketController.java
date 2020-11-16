56e649c8084de598eac408cbe8b68a60cea17a09
Egorpom <Egorpom@yandex.ru>
2020-11-16 18:30:02 +0400

    Refactoring. Part 6. Roles. Get all tickets by role

--- src/main/java/ru/sergei/komarov/med/controller/data/PatientTicketController.java
+++ src/main/java/ru/sergei/komarov/med/controller/data/PatientTicketController.java
@@ -1,9 +1,13 @@
 package ru.sergei.komarov.med.controller.data;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import ru.sergei.komarov.med.model.Doctor;
 import ru.sergei.komarov.med.model.Patient;
 import ru.sergei.komarov.med.model.PatientTicket;
+import ru.sergei.komarov.med.model.Role;
 import ru.sergei.komarov.med.service.PatientTicketService;
 import ru.sergei.komarov.med.service.user.DoctorService;
 import ru.sergei.komarov.med.service.user.PatientService;
@@ -12,6 +16,7 @@ import ru.sergei.komarov.med.util.Utils;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/patientTickets")
@@ -25,19 +30,41 @@ public class PatientTicketController extends BasicDataController<PatientTicket, 
         this.patientService = patientService;
     }
 
+    @Override
+    public List<PatientTicket> getAll() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Role role = authentication.getAuthorities().stream().map(a -> Role.valueOf(a.getAuthority())).collect(Collectors.toList()).get(0);
+        Integer userId = Integer.parseInt(((UserDetails) authentication.getPrincipal()).getUsername());
+        List<PatientTicket> tickets;
+        switch (role) {
+            case PATIENT:
+                tickets = getByPatient(userId);
+                break;
+            case DOCTOR:
+                tickets = getByDoctor(userId);
+                break;
+            case ADMIN:
+                tickets = super.getAll();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + role);
+        }
+        return tickets;
+    }
+
     /* Custom get requests */
 
     //Patient
 
     @GetMapping("/get/patient/{patientId}")
-    public List<PatientTicket> getByPatient(@PathVariable int patientId) {
+    public List<PatientTicket> getByPatient(@PathVariable Integer patientId) {
         Patient patient = patientService.getById(patientId);
         Utils.requireNonNull(patient);
         return ((PatientTicketService) service).getByPatient(patient);
     }
 
     @GetMapping("/get/patient/datetime/{patientId}")
-    public List<PatientTicket> getByPatientAndDateTime(@PathVariable int patientId,
+    public List<PatientTicket> getByPatientAndDateTime(@PathVariable Integer patientId,
                                                        @RequestParam LocalDateTime from,
                                                        @RequestParam LocalDateTime to) {
         Patient patient = patientService.getById(patientId);
@@ -48,14 +75,14 @@ public class PatientTicketController extends BasicDataController<PatientTicket, 
     //Doctor
 
     @GetMapping("/get/doctor/{doctorId}")
-    public List<PatientTicket> getByDoctor(@PathVariable int doctorId) {
+    public List<PatientTicket> getByDoctor(@PathVariable Integer doctorId) {
         Doctor doctor = doctorService.getById(doctorId);
         Utils.requireNonNull(doctor);
         return ((PatientTicketService) service).getByDoctor(doctor);
     }
 
     @GetMapping("/get/doctor/datetime/{doctorId}")
-    public List<PatientTicket> getByDoctorAndDateTime(@PathVariable int doctorId,
+    public List<PatientTicket> getByDoctorAndDateTime(@PathVariable Integer doctorId,
                                                       @RequestParam LocalDateTime from,
                                                       @RequestParam LocalDateTime to) {
         Doctor doctor = doctorService.getById(doctorId);
@@ -66,7 +93,7 @@ public class PatientTicketController extends BasicDataController<PatientTicket, 
     // Patient && Doctor
 
     @GetMapping("/get/patientAndDoctor/{patientId}/{doctorId}")
-    public List<PatientTicket> getByPatientAndDoctor(@PathVariable int patientId, @PathVariable int doctorId) {
+    public List<PatientTicket> getByPatientAndDoctor(@PathVariable Integer patientId, @PathVariable Integer doctorId) {
         Patient patient = patientService.getById(patientId);
         Utils.requireNonNull(patient);
         Doctor doctor = doctorService.getById(doctorId);
@@ -75,8 +102,8 @@ public class PatientTicketController extends BasicDataController<PatientTicket, 
     }
 
     @GetMapping("/get/patientAndDoctor/datetime/{patientId}/{doctorId}")
-    public List<PatientTicket> getByPatientAndDoctorAndDateTime(@PathVariable int patientId,
-                                                                @PathVariable int doctorId,
+    public List<PatientTicket> getByPatientAndDoctorAndDateTime(@PathVariable Integer patientId,
+                                                                @PathVariable Integer doctorId,
                                                                 @RequestParam LocalDateTime from,
                                                                 @RequestParam LocalDateTime to) {
         Patient patient = patientService.getById(patientId);
