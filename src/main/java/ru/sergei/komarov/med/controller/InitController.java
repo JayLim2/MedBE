d77495a66d9b39ddfb9325bce7fa12927cdf6734
JayLim <ddx180874@gmail.com>
2020-11-17 11:14:02 +0400

    Сделан нормальный инит-контроллер

--- src/main/java/ru/sergei/komarov/med/controller/InitController.java
+++ src/main/java/ru/sergei/komarov/med/controller/InitController.java
@@ -11,7 +11,9 @@ import ru.sergei.komarov.med.service.user.UserService;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/init")
@@ -38,18 +40,54 @@ public class InitController {
         initUsers();
     }
 
+    public String getRandomString(List<String> strings) {
+        Random random = new Random();
+        int size = strings.size();
+        if (size == 1) {
+            return strings.get(0);
+        } else {
+            String value = strings.get(random.nextInt(size));
+            strings.remove(value);
+            return value;
+        }
+    }
+
+    private long phone = 7_900_100_00_00L;
+
+    public String getPhone() {
+        return Long.toString(phone++);
+    }
+
     @GetMapping("/users")
     public void initUsers() {
         List<User> users = new ArrayList<>();
         List<DoctorSpecialization> specializations = new ArrayList<>();
         List<DoctorCabinet> cabinets = new ArrayList<>();
 
+        List<String> specs = new ArrayList<>(Arrays.asList(
+                "Врач общей практики", "Терапевт", "Кардиолог",
+                "Эндокринолог", "Стоматолог", "Диагност"
+        ));
+
+        List<String> firstNames = new ArrayList<>(Arrays.asList(
+                "Иван", "Игорь", "Михаил", "Виктор", "Сергей",
+                "Ярополк", "Евпатий", "Никита"
+        ));
+        List<String> lastNames = new ArrayList<>(Arrays.asList(
+                "Иванов", "Петров", "Кузнецов", "Баранов", "Синицын"
+        ));
+        List<String> middleNames = new ArrayList<>(Arrays.asList(
+                "Иванович", "Игоревич", "Михайлович", "Викторович",
+                "Сергеевич", "Ярополкович", "Евпатиевич", "Никитич"
+        ));
+
         for (int i = 1; i <= 5; i++) {
             Patient patient = new Patient();
-            patient.setPhone("patient" + i);
+            patient.setPhone(getPhone());
             patient.setPassword(passwordEncoder.encode("root"));
-            patient.setFirstName("Пациент");
-            patient.setLastName("(" + i + ") Пациентов");
+            patient.setFirstName(getRandomString(firstNames));
+            patient.setLastName(getRandomString(lastNames));
+            patient.setMiddleName(getRandomString(middleNames));
             patient.setRole(Role.PATIENT);
             patient.setRegistrationAddress("Адрес " + i);
             patient.setBirthday(LocalDate.now());
@@ -59,19 +97,20 @@ public class InitController {
 
         for (int i = 1; i <= 3; i++) {
             DoctorSpecialization specialization = new DoctorSpecialization();
-            specialization.setName("Офтальпатологоанатом " + i);
+            specialization.setName(getRandomString(specs));
             specializations.add(specialization);
 
             DoctorCabinet cabinet = new DoctorCabinet();
-            cabinet.setName("Морг " + i);
+            cabinet.setName(Integer.toString(i));
             cabinet.setRecommendedDoctorsCount(i);
             cabinets.add(cabinet);
 
             Doctor doctor = new Doctor();
-            doctor.setPhone("doctor" + i);
+            doctor.setPhone(getPhone());
             doctor.setPassword(passwordEncoder.encode("root"));
-            doctor.setFirstName("Врач");
-            doctor.setLastName("(" + i + ") Лечилов");
+            doctor.setFirstName(getRandomString(firstNames));
+            doctor.setLastName(getRandomString(lastNames));
+            doctor.setMiddleName(getRandomString(middleNames));
             doctor.setRole(Role.DOCTOR);
             doctor.setWorkingNow(i % 2 == 1);
             doctor.setSpecialization(specialization);
@@ -81,10 +120,11 @@ public class InitController {
 
         for (int i = 1; i <= 1; i++) {
             User user = new User();
-            user.setPhone("admin" + i);
+            user.setPhone(getPhone());
             user.setPassword(passwordEncoder.encode("root"));
-            user.setFirstName("Админ");
-            user.setLastName("(" + i + ") Админов");
+            user.setFirstName(getRandomString(firstNames));
+            user.setLastName(getRandomString(lastNames));
+            user.setMiddleName(getRandomString(middleNames));
             user.setRole(Role.ADMIN);
             users.add(user);
         }
